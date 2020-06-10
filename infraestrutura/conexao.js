ffb2d370fe255707ab85f65ee75452d81fd6d762
marcoavfcc01121979 <marcoavfcc@gmail.com>
2020-06-10 09:01:58 -0300

    first commit

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -4,8 +4,9 @@ const conexao = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: 'admin',
-  database: 'agenda-petshop'
+  password: '29121950',
+  database: 'agenda-petshop1',
+  multipleStatements: true
 })
 
 module.exports = conexao
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const customExpress = require('./config/custom-express')
+const { GraphQLServer } = require('graphql-yoga') 
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/database/tabelas')
-
-const app = customExpress()
+const Operacoes = require('./infraestrutura/operations')
 
 conexao.connect(erro => {
   if (erro) {
@@ -14,6 +13,29 @@ conexao.connect(erro => {
   Tabelas.init(conexao)
 })
 
-app.listen(4000, () => {
-  console.log('Servidor rodando na porta 4000')
+const Clientes = new Operacoes('cliente')
+const Pets = new Operacoes('pet')
+
+const resolvers = {
+  Query: {
+    status: () => "Servidor rodando!",
+    clientes: () => Clientes.lista(),
+    cliente: (root, { id }) => Clientes.buscaPorId(id),
+    pets: () => Pets.lista(),
+    pet: (root, { id }) => Pets.buscaPorId(id)
+  }, 
+  Mutation: {
+    adicionarCliente: (root, params) => Clientes.adiciona(params),
+    atualizarCliente: (root, params) => Clientes.atualiza(params),
+    deletarCliente: (root, { id }) => Clientes.deleta(id),
+    adicionarPet: (root, params) => Pets.adiciona(params),
+    atualizarPet: (root, params) => Pets.atualiza(params),
+    deletarPet: (root, { id }) => Pets.deleta(id)
+  }
+} 
+const servidor = new GraphQLServer({
+  resolvers,
+  typeDefs: './schema.graphql'
 })
+
+servidor.start(() => console.log('servidor ouvindo.'))
